e1c9f0714d4c5baf59b3f7d1e5e73dbdf84296c6
Michael Reeves <mreeves68@yahoo.com>
2016-04-18 15:13:07 -0600

    symbolize_names of response body in test

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -6,14 +6,14 @@ describe Api::V1::ItemsController, type: :controller do
   it '#index' do
     get :index, format: :json
 
-    items = JSON.parse( response.body )
+    items = JSON.parse( response.body, symbolize_names: true )
     item  = items.first
 
     expect(response).to have_http_status :success
     
-    expect(items.count).to         eq 2
-    expect(item['name']).to        eq 'Lannister'
-    expect(item['description']).to eq 'My uncle is my dad'
+    expect(items.count).to        eq 2
+    expect(item[:name]).to        eq 'Lannister'
+    expect(item[:description]).to eq 'My uncle is my dad'
   end
 
 end
