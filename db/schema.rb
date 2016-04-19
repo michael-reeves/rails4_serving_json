71a84b99bfaabcf5545e77bda405466e3d5a6cb8
Michael Reeves <mreeves68@yahoo.com>
2016-04-19 16:42:04 -0600

    Generate UsersController and User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160418170246) do
+ActiveRecord::Schema.define(version: 20160419223659) do
 
   create_table "items", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
