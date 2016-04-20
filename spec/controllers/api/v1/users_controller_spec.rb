55c8793476c5f13cfbdd4f998c17bef593a2c67e
Michael Reeves <mreeves68@yahoo.com>
2016-04-20 09:15:36 -0600

    Add UsersController#index with spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
-RSpec.describe Api::V1::UsersController, type: :controller do
+describe Api::V1::UsersController, type: :controller do
+  fixtures :users
+
+  context '#index' do
+    it 'successfully returns all users' do
+      get :index, format: :json
+
+      users = JSON.parse( response.body, symbolize_names: true )
+      user  = users.first
+
+      expect( response ).to     have_http_status :success
+
+      expect( users.count ).to  eq 2
+      expect( user[:name] ).to  eq 'Tyrion Lannister'
+      expect( user[:email] ).to eq 'tyrion@lannister.com'
+    end
+  end
 
 end
