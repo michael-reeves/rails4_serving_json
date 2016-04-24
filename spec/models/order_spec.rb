d7e17ae9a2d6033d4109041b4a197dd5c34b4c4b
Michael Reeves <mreeves68@yahoo.com>
2016-04-24 17:24:13 -0600

    Add model specs for user/orders association

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Order, type: :model do
+  before(:each) do
+    user   = User.create!( name: 'Jon Snow', email: 'jon.snow@stark.org' )
+    @order = Order.create!( amount: 10.0, user_id: user.id )
+  end
+
+  it 'has a user' do
+    expect( @order.user.name  ).to eq 'Jon Snow'
+    expect( @order.user.email ).to eq 'jon.snow@stark.org'
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe User, type: :model do
+  fixtures :users
+
   let(:lannister) { User.first }
   
   it '#name exists' do
@@ -9,5 +11,14 @@ describe User, type: :model do
 
   it '#email exists' do
     expect( lannister.email ).to eq 'tyrion@lannister.com'
+  end
+
+  it 'has orders' do
+    lannister.orders << Order.create!( amount: 100.0 )
+    lannister.orders << Order.create!( amount: 599.99 )
+
+    expect( lannister.orders.count ).to        eq 2
+    expect( lannister.orders.first.amount ).to eq 100.0
+    expect( lannister.orders.last.amount  ).to eq 599.99
   end
 end
